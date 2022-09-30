bfad0d6935df1ca6b35d91e8ff96af2d4edb0753
Виталий <@DESKTOP-NTOVGEQ>
2022-10-01 02:45:50 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double quontity = 100000000; // кол-во населения
+            double area = 60000; //площадь территории
+            double density = Math.Round(quontity / area, 2); //формула для вычисления плотности
+            Console.WriteLine("Плотность населения ДНР равна {0} чел/км", density); //вывод плотности населения
+            Console.WriteLine(new string('-', 50));
+        }
+    }
+}
